edd3f4dcd741fee2737b8e3a618b3c02b6af586c
Raka Prasetya Pratama <madeayurenapramudya@gmail.com>
2023-03-03 14:52:57 +0000

    nad: Initial GSI Patches for NusantarProject Ten (EOL)

---
+++ nad.mk
@@ -0,0 +1,13 @@
+$(call inherit-product, vendor/nusantara/config/common.mk)
+$(call inherit-product, vendor/nusantara/config/BoardConfigSoong.mk)
+$(call inherit-product, vendor/nusantara/config/BoardConfigNAD.mk)
+$(call inherit-product, device/nusantara/sepolicy/common/sepolicy.mk)
+-include vendor/nusantara/build/core/config.mk
+TARGET_NO_KERNEL_OVERRIDE := true
+TARGET_NO_KERNEL_IMAGE := true
+SELINUX_IGNORE_NEVERALLOWS := true
+
+TARGET_USES_PREBUILT_VENDOR_SEPOLICY := true
+TARGET_HAS_FUSEBLK_SEPOLICY_ON_VENDOR := true
+
+TARGET_FACE_UNLOCK_SUPPORTED := true
